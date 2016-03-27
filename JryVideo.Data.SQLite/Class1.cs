b3bb19b90f8a4a7f49108fd016c89c8b2916fe2c
赖巍 <skyoflw@gmail.com>
2016-03-27 22:35:20 +0800

    chore: init sqlite db support

---
+++ JryVideo.Data.SQLite/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JryVideo.Data.SQLite
+{
+    public class Class1
+    {
+    }
+}
